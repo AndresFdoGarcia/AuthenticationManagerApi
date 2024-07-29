756664ea25bdca0819d5adb1d6880b761e46499a
AndresFdoGarcia <75505264+AndresFdoGarcia@users.noreply.github.com>
2024-07-28 21:32:36 -0500

    Update authmanager.sql

--- Anexos/Script/authmanager.sql
+++ Anexos/Script/authmanager.sql
@@ -1,18 +1,30 @@
-CREATE SCHEMA `authmanager` ;
-
-CREATE TABLE `authmanager`.`users` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
+CREATE TABLE `authmanager`.`persons` (
+  `id` CHAR(36) PRIMARY KEY NOT NULL,
+  `idnumber` VARCHAR(20) NOT NULL,
+  `idtype` VARCHAR(5) NOT NULL,  
   `email` VARCHAR(45) NOT NULL,
   `firstname` VARCHAR(45) NOT NULL,
   `lastname` VARCHAR(45) NOT NULL,
-  `rol` VARCHAR(15) NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE,
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  `creationdate` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  `fullid` VARCHAR(255) AS (REPLACE(CONCAT(idtype,idnumber),' ','')),
+  `fullname` VARCHAR(255) AS (REPLACE(CONCAT(firstname,lastname),' ','')));
 
-INSERT INTO `authmanager`.`users` (`username`, `password`, `email`, `firstname`, `lastname`, `rol`) VALUES 
-('afperea', 'TmN@x', 'andres.perea@hotmail.com', 'Andres', 'Garcia', 'Admin');
+
+CREATE TABLE `authmanager`.`users` (
+    `id` CHAR(36) PRIMARY KEY,
+    `username` VARCHAR(45) NOT NULL,
+    `password` VARCHAR(45) NOT NULL,
+    `creationdate` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (Id) REFERENCES `authmanager`.`persons`(Id)
+);
+
+DELIMITER //
+use `authmanager`;
+CREATE PROCEDURE GetAllUsers()
+BEGIN
+    SELECT p.firstname, p.lastname, p.email, u.username 
+    FROM persons p
+    JOIN users u ON p.id = u.id;
+END //
+
+DELIMITER ;
